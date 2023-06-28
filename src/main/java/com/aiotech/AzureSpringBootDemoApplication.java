691cb4dbab4253e6fe8ca18d2d8f89c0330d9bdb
aiotechhub <aiotechhub@gmail.com>
2023-06-28 18:47:07 +0530

    added rest controller

--- src/main/java/com/aiotech/AzureSpringBootDemoApplication.java
+++ src/main/java/com/aiotech/AzureSpringBootDemoApplication.java
@@ -2,12 +2,20 @@ package com.aiotech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AzureSpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureSpringBootDemoApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats! you have successfully deployed your application in Azure Cloud!";
 	}
 
 }
