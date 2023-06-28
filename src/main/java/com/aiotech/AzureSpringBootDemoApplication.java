41b5dfdb21d8789b73946ab121e2884e2143738e
aiotechhub <aiotechhub@gmail.com>
2023-06-28 18:36:56 +0530

    added skeleton

---
+++ src/main/java/com/aiotech/AzureSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aiotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringBootDemoApplication.class, args);
+	}
+
+}
